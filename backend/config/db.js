05cd56c9e9c2b8927e878a65813e454434f17634
divybagora10 <divybagora1122@gmail.com>
2024-10-09 23:05:38 +0530

    Atlas1

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDb = async () =>{
     try {
         // const connection = await mongoose.connect("mongodb://localhost:27017/projectDB")
-        const connection = await mongoose.connect("mongodb+srv://divybagora1122:WQ8AykTIXjxusY7m@cluster1.wrxti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+        const connection = await mongoose.connect("mongodb+srv://User1:1vzKXc6TElv2ILOe@cluster1.wrxti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+        // const connection = await mongoose.connect("mongodb+srv://divybagora1122:WQ8AykTIXjxusY7m@cluster1.wrxti.mongodb.net/?retryWrites=true&w=majority&ssl=true")
         console.log("db connected");
     } catch (error) {
         console.log(error);
